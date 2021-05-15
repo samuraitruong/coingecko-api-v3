0051419b77bac1a4bf306932fbc07f3d480929f0
Truong Nguyen <tnguyen@demystdata.com>
2021-05-15 22:00:03 +1000

    update jest coverage

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,10 @@ module.exports = {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 65,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
